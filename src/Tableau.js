a6b8b6f3f33ef631c8cc66602d73416071af3c8d
Quentin-Allmn <92529322+Quentin-Allmn@users.noreply.github.com>
2022-01-10 14:31:21 +0100

    m

--- src/Tableau.js
+++ src/Tableau.js
@@ -15,11 +15,13 @@ class Tableau1 extends Phaser.Scene{
 
         this.height=500;
 
-        this.haut=this.add.image(0,0,this.width,20,'carre').setOrigin(0,0);
+        this.haut=this.physics.add.image(0,0,'carre').setOrigin(0,0);
+        this.haut.setDisplayOrigin(this.width,20);
 
-        this.bas=this.add.image(20,150,this.width-20,20,'carre').setOrigin(0,0);
+        this.bas=this.physics.add.image(20,150,'carre').setOrigin(0,0);
+        this.bas.setDisplayOrigin(this.width,20);
 
-        this.balle=this.add.image(20,50,'cercle').setOrigin(0,0);
+        this.balle=this.physics.add.image(this.width/2,this.width/2,'cercle').setOrigin(0,0);
         this.balle.setDisplayOrigin(20,20);
     }
 
