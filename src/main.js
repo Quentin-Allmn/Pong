24ce55ab0e5ddf911c08861e468cb91c62350c1a
Quentin-Allmn <92529322+Quentin-Allmn@users.noreply.github.com>
2022-01-10 13:25:38 +0100

    m

---
+++ src/main.js
@@ -0,0 +1,15 @@
+let gameConfig = {
+    type: Phaser.AUTO,
+    width: 1400,
+    height: 760,
+    backgroundColor: '#ffffff',
+    parent: 'game',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: new Tableau1()
+};
+let game = new Phaser.Game(gameConfig);
