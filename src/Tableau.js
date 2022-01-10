6576bc4124d7f35a2ce5a5776a9a490245d700b9
Quentin-Allmn <92529322+Quentin-Allmn@users.noreply.github.com>
2022-01-10 14:40:55 +0100

    m

--- src/Tableau.js
+++ src/Tableau.js
@@ -15,15 +15,23 @@ class Tableau1 extends Phaser.Scene{
 
         this.height=500;
 
-        this.haut=this.physics.add.image(0,0,'carre').setOrigin(0,0);
-        this.haut.setDisplayOrigin(this.width,20);
+        this.haut=this.physics.add.sprite(0,0,'carre').setOrigin(0,0);
+        this.haut.setDisplaySize(this.width,20);
+        this.haut.body.setAllowGravity(false);
 
-        this.bas=this.physics.add.image(20,150,'carre').setOrigin(0,0);
-        this.bas.setDisplayOrigin(this.width,20);
+        this.bas=this.physics.add.sprite(20,this.height-20,'carre').setOrigin(0,0);
+        this.bas.setDisplaySize(this.width,20);
+        this.bas.body.setAllowGravity(false);
+        this.bas.setImmovable(true);
 
-        this.balle=this.physics.add.image(this.width/2,this.width/2,'cercle').setOrigin(0,0);
-        this.balle.setDisplayOrigin(20,20);
+        this.balle=this.physics.add.sprite(this.width/2,this.width/2,'cercle').setOrigin(0,0);
+        this.balle.setDisplaySize(20,20);
+        this.balle.body.setBounce(0.9,1);
+
+        this.physics.add.collider(this.balle,this.bas);
+        this.physics.add.collider(this.balle,this.haut);
     }
+
 
     update(){
 
